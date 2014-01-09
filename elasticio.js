db1c708d349ecd207fe10376bbb357a51642ce32
Hajo Eichler <hajo.eichler@commercetools.de>
2014-01-09 14:25:09 +0100

    Add some console logs for testing elastic.io integration.

--- elasticio.js
+++ elasticio.js
@@ -1,6 +1,9 @@
 StockXmlImport = require('./main').StockXmlImport
 
-exports.process = function(msg, cfg, cb, snapshot) {
+exports.process = function(msg, cfg, next, snapshot) {
+  console.log("msg: %j", msg)
+  console.log("cfg: %j", cfg)
+  console.log("next: " + next)
   config = {
     client_id: cfg.sphereClientId,
     client_secret: cfg.sphereClientSecret,
@@ -9,5 +12,5 @@ exports.process = function(msg, cfg, cb, snapshot) {
   var im = new StockXmlImport({
     config: config
   });
-  im.elasticio(msg, cfg, cb, snapshot);
+  im.elasticio(msg, cfg, next, snapshot);
 }
