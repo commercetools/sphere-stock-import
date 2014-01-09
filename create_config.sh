f4c7e0b714c8d99d1a79b7259cb91d0d178fefe2
Hajo Eichler <hajo.eichler@commercetools.de>
2014-01-09 12:17:54 +0100

    Adjust config to enabe logentries logging during tests.

--- create_config.sh
+++ create_config.sh
@@ -6,7 +6,7 @@ exports.config = {
   client_id: "${SPHERE_CLIENT_ID}",
   client_secret: "${SPHERE_CLIENT_SECRET}",
   project_key: "${SPHERE_PROJECT_KEY}",
-  logentries_token: "${LOGENTRIES_TOKEN}",
-  timeout: 60000
 }
+
+exports.logentries_token = "${LOGENTRIES_TOKEN}"
 EOF
