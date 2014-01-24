8643381953f91f1d33a55a19e7fa2e9b8dd6a20e
Hajo Eichler <hajo.eichler@commercetools.de>
2014-01-24 08:47:14 +0100

    Extract from package.json what libs to add in production brunch.

--- deploy-to-elastic.sh
+++ deploy-to-elastic.sh
@@ -3,20 +3,20 @@
 set -e
 
 rm -rf lib
-rm -rf node_modules
 
 npm version patch
 git checkout production
 git merge master
 
-npm install --production
 grunt build
 git add -f lib/
-git add -f node_modules/
+libs=$(cat package.json | jq -r '.dependencies' | grep ':' | cut -d: -f1 | tr -d " " | tr -d '"')
+for lib in $libs; do
+    git add -f node_modules/$lib
+done
 git commit -m "Add generated code and runtime dependencies for elastic.io environment."
 git push origin production
 
 git checkout master
 npm version patch
 git push origin master
-npm install
