82e8df9dceb9e0c0a40a095a1b36f0a4d9c751fe
Hajo Eichler <hajo.eichler@commercetools.de>
2014-01-24 08:34:03 +0100

    Do also push runtime dependencies to production branch.

--- deploy-to-elastic.sh
+++ deploy-to-elastic.sh
@@ -3,16 +3,20 @@
 set -e
 
 rm -rf lib
+rm -rf node_modules
 
 npm version patch
 git checkout production
 git merge master
 
+npm install --production
 grunt build
 git add -f lib/
-git commit -m "Add generated code for elastic.io environment."
+git add -f node_modules/
+git commit -m "Add generated code and runtime dependencies for elastic.io environment."
 git push origin production
 
 git checkout master
 npm version patch
 git push origin master
+npm install
