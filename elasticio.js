6d3e73d69ea4a21837ff7d108cd8260b26381037
Hajo Eichler <hajo.eichler@commercetools.de>
2014-03-11 09:09:32 +0100

    Nicer output.

--- elasticio.js
+++ elasticio.js
@@ -1,7 +1,7 @@
 StockXmlImport = require('./main').StockXmlImport
 
 exports.process = function(msg, cfg, next, snapshot) {
-  console.log("msg: %j", msg)
+  console.log("Got elastic.io msg: %j", msg)
   config = {
     client_id: cfg.sphereClientId,
     client_secret: cfg.sphereClientSecret,
