63d102a061e13b4bad06dc24600df60c563eeb74
Hajo Eichler <hajo.eichler@commercetools.de>
2014-04-03 11:18:49 +0200

    Adjust loglevel and user agent for elastic integration.

--- elasticio.js
+++ elasticio.js
@@ -1,3 +1,4 @@
+package_json = require './package.json'
 StockXmlImport = require('./main').StockXmlImport
 
 exports.process = function(msg, cfg, next, snapshot) {
@@ -6,7 +7,12 @@ exports.process = function(msg, cfg, next, snapshot) {
     client_id: cfg.sphereClientId,
     client_secret: cfg.sphereClientSecret,
     project_key: cfg.sphereProjectKey,
-    timeout: 60000
+    timeout: 60000,
+    user_agent: "#{package_json.name} - elasticio - #{package_json.version}"
+    logConfig: {
+      levelStream: 'warn',
+      levelFile: 'error'
+    }
   };
   var im = new StockXmlImport({
     config: config
